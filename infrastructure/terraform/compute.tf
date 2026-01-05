aeb3fa7e52a4aac8ace3efaf397c840834d69a6e
abiodun oyebanji <stepheng323@gmail.com>
2026-01-05 16:11:36 +0100

    chore(terraform): reset availability domain to index 0

--- infrastructure/terraform/compute.tf
+++ infrastructure/terraform/compute.tf
@@ -9,7 +9,7 @@ data "oci_core_images" "ubuntu_images" {
 }
 
 resource "oci_core_instance" "banking_server" {
-  availability_domain = data.oci_identity_availability_domains.ads.availability_domains[0].name
+  availability_domain = data.oci_identity_availability_domains.ads.availability_domains[2].name
   compartment_id      = var.compartment_ocid
   display_name        = "banking-agent-server"
   shape               = var.instance_shape
